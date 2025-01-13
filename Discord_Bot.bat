39dc1c12acd50e27daa504f13aa991077e343273
Marwan Mostafa <marawan206@gmail.com>
2025-01-13 04:58:18 +0200

    Added batch file to run bot

---
+++ Discord_Bot.bat
@@ -0,0 +1,10 @@
+@echo off
+:: Navigate to the directory containing your bot
+cd /d ".\TL Bot"
+
+:: Activate Python if you have a virtual environment
+:: call "path\to\venv\Scripts\activate.bat"
+
+:: Run the bot script
+python TTM.py
+pause
